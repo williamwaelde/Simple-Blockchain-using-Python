171e6d543991d3819435a76a379f788cda16b107
William Wälde <william.waelde@mni.th.de>
2021-10-31 13:57:10 +0100

    working chain.

--- blockchain.py
+++ blockchain.py
@@ -3,7 +3,7 @@ import hashlib
 
 
 # object representing chain links
-class block:
+class Block:
     # constructor:
     # nr = transaction number /block number
     # previous_hash = hash value of previous block
@@ -13,19 +13,19 @@ class block:
         self.data = data
         self.previous_hash = previous_hash
         # hash of current block:
-        # ading to nr the data and previous_hash
+        # adding to nr the data and previous_hash
         # everything will be hashed together in unique hash value
         # using sha256 hash function with hexdigest containing only hexadecimal digits
         self.hash = hashlib.sha256((str(nr) + data + previous_hash).encode()).hexdigest()
 
-        # make it human readable
-        def __str__(self):
-            # format() method formats the specified value(s) and insert them inside the string's placeholder
-            return '{}:::{};{}'.format(self.nr, self.hash, self.data)
+    # improve readability
+    def __str__(self):
+        # format() method formats the specified value(s) and insert them inside the string's placeholder
+        return '{}:::{};{}'.format(self.nr, self.hash, self.data)
 
 
 # object representing blockchain
-class blockchain:
+class Blockchain:
     # constructor:
     def __init__(self):
         # genesis Block is the name given to the first block a blockchain
@@ -33,11 +33,48 @@ class blockchain:
         # nr = 0
         # data = Genesis
         # pseudo hash value: '0'*64 because SHA256 is using hexdigest() with 64 characters
-        genesisBlock = block(0, 'Genesis', '0' * 64)
+        genesisBlock = Block(0, 'Genesis', '0'*64)
 
         # list of blocks stating with genisis block
-        self.blockList = [genesisBlock]
+        self.blocks = [genesisBlock]
 
-        # adding blocks to chain using information of block list
-        def append(self, data):
-            self.blockList.append()
+    # adding blocks to chain using information of block list
+    def append(self, data):
+        self.blocks.append(
+            Block(
+                # current number of blocks from blockList
+                # first real transaction starting number 1
+                len(self.blocks),
+                data,
+                # hash value of previous block
+                # -1 because current block is lat in list and previous is last -1
+                self.blocks[len(self.blocks)-1].hash
+            )
+        )
+
+
+    # improve readability
+    def __str__(self):
+        res = ''
+        for block in self.blocks:
+            # implicit calling string method of block class
+            res += '{}\n'.format(block)
+            return res
+
+# testing blockchain
+bc = Blockchain()
+
+#alice sending bob 0.002 bitcoin
+bc.append('Alice sendet Bob <0.002 BTC>')
+
+bc.append('<Fred> sendet <Jack> <0.087 BTC>')
+
+bc.append('<Donald> sendet <Vladimir> <1.2 BTC>')
+
+bc.append('<Max> sendet <Jessica> <0.6 BTC>')
+
+#print blockchain
+for x in range(len(bc.blocks)):
+    print (bc.blocks[x])
+
+
